c39bd334cad2553415a574879c66131c294b0a78
Muhammad Kuswari <muh.kuswari10@gmail.com>
2021-05-10 02:51:09 +0800

    working in project: pricing

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import propTypes from "prop-types";
 
 export default function Button(props) {
-    const className = ['button']
-    if(props.isPrimary)
-        className.push("button-primary")
-    if(props.isWideMobile)
-        className.push("button-wide-mobile")
-    if(props.isSmall)
-        className.push("button-sm")
-    return <button className={className.join(" ")}>{props.children}</button>
+  const className = ["button"];
+  if (props.isPrimary) className.push("button-primary");
+  if (props.isWideMobile) className.push("button-wide-mobile");
+  if (props.isSmall) className.push("button-sm");
+  if (props.isBlock) className.push("button-block");
+  return <button className={className.join(" ")}>{props.children}</button>;
 }
 
 Button.propTypes = {
-    isPrimary: PropTypes.bool,
-    isWideMobile: PropTypes.bool,
-    isSmall: PropTypes.bool,
-}
+  isPrimary: propTypes.bool,
+  isWideMobile: propTypes.bool,
+  isBlock: propTypes.bool,
+  isSmall: propTypes.bool,
+};
